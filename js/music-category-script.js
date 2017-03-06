053f38b1e77b9b55bbdd6430ebaddbed32ca1b52
jayaperkins <jayaperkins@gmail.com>
2017-03-05 21:38:28 -0800

    jquery update

--- js/music-category-script.js
+++ js/music-category-script.js
@@ -1,5 +1,7 @@
 $(function() {
 
+    var $categoryContainer = $('.category-container');
+
     var $categoryOne = $('.category-one');
 
     var $categoryTwo = $('.category-two');
@@ -14,11 +16,15 @@ $(function() {
 
         $orchestrationList.slideDown(500);
 
+        $categoryContainer.addClass('main-margin-add');
+
     });
 
     $orchestrationList && $categoryOne.on('mouseleave', function() {
 
         $orchestrationList.slideUp(500);
+
+        $categoryContainer.removeClass('main-margin-add');
 
     });
 
@@ -27,11 +33,15 @@ $(function() {
 
         $choralList.slideDown(500);
 
+        $categoryContainer.addClass('main-margin-add');
+
     });
 
     $choralList && $categoryTwo.on('mouseleave', function() {
 
         $choralList.slideUp(500);
+
+        $categoryContainer.removeClass('main-margin-add');
 
     });
 
