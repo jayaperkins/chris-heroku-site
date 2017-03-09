c4ebddb8693f824bf66fc4e96dbcabc013e921b4
jayaperkins <jayaperkins@gmail.com>
2017-03-09 14:38:36 -0800

    updated https

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
 <?php include_once("home.html"); ?>
+
+if (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] !== 'on') {
+    if(!headers_sent()) {
+        header("Status: 301 Moved Permanently");
+        header(sprintf(
+            'Location: https://%s%s',
+            $_SERVER['HTTP_HOST'],
+            $_SERVER['REQUEST_URI']
+        ));
+        exit();
+    }
+}
