d8a94aba7499c0ed851d90c28eef291df206035f
jayaperkins <jayaperkins@gmail.com>
2017-03-10 11:15:28 -0800

    update

--- home.php
+++ home.php
@@ -2,6 +2,12 @@
 <html lang="en">
 
 <head>
+    <?php if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
+    $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $redirect);
+    exit();
+} ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-site-verification" content="HZLCiyutFx4RDPBIySul0CvGWc2gP3VYmQeqQahQY_Q" />
--- index.php
+++ index.php
@@ -1,10 +1,5 @@
 <?php
 
-include_once("home.html");
-
-if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
-    $redirect = 'https://www.enloemusic.com';
-    exit();
-}
+include_once("home.php");
 
 ?>
