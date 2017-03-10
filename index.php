a117ff43b0e4f4a2c1da7fa1060b29b845c55f2a
jayaperkins <jayaperkins@gmail.com>
2017-03-10 11:11:08 -0800

    update

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
-include_once("home.html");
-
 if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
     $redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
     exit();
 }
+
+include_once("home.html");
 
 ?>
