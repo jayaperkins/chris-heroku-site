e1035b754733c0442ec897b52dc840c1e1128d7d
jayaperkins <jayaperkins@gmail.com>
2017-03-10 11:13:20 -0800

    update

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 
+include_once("home.html");
+
 if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
     $redirect = 'https://www.enloemusic.com';
     exit();
 }
-
-include_once("home.html");
 
 ?>
